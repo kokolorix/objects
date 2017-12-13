98ca78da47c6a9dc006ebae2cd4e3a29a6739663
srdev <srdev@4254f4cc-1a69-a04a-b4cb-ca1b0837f616>
2017-12-13 22:21:51 +0000

    Members Test
    git-svn-id: https://paps-pc/svn/objects/trunk@72 4254f4cc-1a69-a04a-b4cb-ca1b0837f616

--- UnitTest/MemberPropertyTests.cpp
+++ UnitTest/MemberPropertyTests.cpp
@@ -1,7 +1,10 @@
 #include "stdafx.h"
 #include "MemberPropertyTests.h"
-#include <assert.h>
-#include <functional>
+#include "Value.h"
+#undef max
+#include <limits>
+//#include <assert.h>
+//#include <functional>
 
 using namespace obj;
 
@@ -11,12 +14,12 @@ namespace
 }
 
 using std::function;
+template<class T> struct Nothing { T nothing_; };
 
-template<class T, class HostT, T HostT::*member>
+template<class T, class HostT = Nothing<T>, T HostT::*member = &Nothing<T>::nothing_>
 struct ReadMember
 {
 	using Getter = function<T()>;
-	//using Setter = function<T(const T&)>;
 
 	ReadMember(HostT* host) 
 	{
@@ -25,37 +28,102 @@ struct ReadMember
 			return (*host).*member;
 		};
 	}
-	operator T () const { return getter_(); }
+	ReadMember(const T& m) 
+	{
+		getter_ = [m]() 
+		{ 
+			return m;
+		};
+	}
+	operator T () const 
+	{
+		return getter_();
+	}
+	template<typename OT>
+	T operator = (const OT& m)
+	{
+		static_assert(0, "bullshit!");
+	}
+	template<typename OT> inline bool operator == (const OT& o) { return getter_() == o; }
+	template<typename OT> inline bool operator != (const OT& o) { return getter_() != o; }
+	template<typename OT> inline bool operator < (const OT& o) { return getter_() < o; }
+	template<typename OT> inline bool operator > (const OT& o) { return getter_() > o; }
+	template<typename OT> inline bool operator <= (const OT& o) { return getter_() <= o; }
+	template<typename OT> inline bool operator >= (const OT& o){ return getter_() >= o; }
+
+protected:
 	Getter getter_;
 };
+template<class T, class HostT, T HostT::*member>
+struct WriteMember : public ReadMember<T,HostT,member>
+{
+	using Setter = function<T(const T&)>;
+	WriteMember(HostT* host) : ReadMember(host)
+	{
+		setter_ = [host](const T& m)
+		{
+			return (host->*member) = m;
+		};
+	}
+	T operator = (const T& m)
+	{
+		return setter_(m);
+	}
+private:
+	Setter setter_;
+};
 
-template<class T, class HostT, T HostT::*member>
-bool operator == (const ReadMember<T,HostT,member>& l, const T& r) { return l.operator T() == r; }
-template<class T, class HostT, T HostT::*member>
-bool operator == (const ReadMember<T,HostT,member>& l, const char* r) { return l.operator T() == r; }
-template<class T, class HostT, T HostT::*member>
-bool operator == (const T& l, const ReadMember<T,HostT,member>& r) { return l == r.operator T(); }
-template<class T, class HostT, T HostT::*member>
-bool operator == (const char* l, const ReadMember<T,HostT,member>& r) { return l == r.operator T(); }
+template<typename OT, class T, class HostT, T HostT::*member>
+bool operator == (const OT& l, const ReadMember<T,HostT,member>& r) { return l == r.operator T(); }
+template<typename OT, class T, class HostT, T HostT::*member>
+bool operator != (const OT& l, const ReadMember<T, HostT, member>& r) { return l != r.operator T(); }
+template<typename OT, class T, class HostT, T HostT::*member>
+bool operator < (const OT& l, const ReadMember<T, HostT, member>& r) { /*if (std::is_signed<OT>::value);*/ return l < r.operator T(); }
+template<typename OT, class T, class HostT, T HostT::*member>
+bool operator > (const OT& l, const ReadMember<T, HostT, member>& r) { return l > r.operator T(); }
+template<typename OT, class T, class HostT, T HostT::*member>
+bool operator <= (const OT& l, const ReadMember<T, HostT, member>& r) { return l <= r.operator T(); }
+template<typename OT, class T, class HostT, T HostT::*member>
+bool operator >= (const OT& l, const ReadMember<T, HostT, member>& r) { return l >= r.operator T(); }
 
 struct Host
 {	
+	uint32_t id_;
 	bool ready_;
 	String name_;
-	ReadMember<bool, Host, &Host::ready_> Ready = {this};
-	ReadMember<String, Host, &Host::name_> Name = {this};
+	ReadMember<uint32_t, Host, &Host::id_> Id = { this };
+	ReadMember<bool, Host, &Host::ready_> Ready = { this };
+	WriteMember<String, Host, &Host::name_> Name = { this };
+	ReadMember<double> Pi = { 3.14 };
+	ReadMember<char*> ClassName = { "Host" };
+	ReadMember<UuId> Guid = { generateId() };
+	ReadMember<uint64_t> BigOne = { 33 };
 };
-
-
-
 
 obj::TestResult obj::Test::MemberPropertyTests::runTest()
 {
-	Host h = { true, "theName" };
-	assert(h.Ready);
+	Host h = { 17,  true, "theName" };
+	assert(h.Ready != false);
 	bool ready = h.Ready;
 	String name = h.Name;
+	assert(h.Id == 17);
+	//h.Pi = 2.77;
+	assert(0 != h.Id);
+	assert(20 < h.Id);
+	//assert(h.Id < 20);
+	//assert(h.Id > 10);
+	assert(h.Pi == 3.14);
+	assert(h.ClassName == "Host");
+	assert("Host" == h.ClassName);
+	assert(h.ClassName == h.ClassName);
+	assert(name == h.Name);
+	assert(h.Name == name);
 	assert(h.Name == "theName");
-	assert("theName" == /*(String)*/h.Name);
+	assert("theName" == h.Name);
+	//assert("theName" == /*(String)*/h.Name);
+	h.Name = "anotherName";
+	assert(h.Name == "anotherName");
+
+	assert(h.BigOne < std::numeric_limits<uint64_t>::max());
 	return TestResult::Successful;
 }
